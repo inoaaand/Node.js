3ee7b6e0f4e08ba7d6540b12cac4739b66bd5dc1
isnoand <20233005420@estudantes.ifpr.edu.br>
2025-08-04 11:24:27 -0300

    UPDATE FILES

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+import { obterInfoSistema } from './testeOS';
 const calc = require('./calculadora');
 const calc1 = require('./calculadora01');
+
 
 console.log("Soma: ", calc.soma(2, 2));
 
@@ -8,6 +10,12 @@ console.log("Multiplicação: ", calc.multiplicar(2, 6));
 console.log("Subtração: ", calc1.subtrair(2, 10));
 
 console.log("Divisão: ", calc1.dividir(2, 10));
+
+const info = obterInfoSistema();
+console.log("Informações do Sistema Operacional:\n");
+console.table(info);
+
+
 
 
 
