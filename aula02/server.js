e3d0b2fb672be89b160664433e5a7a04355ed4f3
isnoand <20233005420@estudantes.ifpr.edu.br>
2025-08-11 10:58:15 -0300

    update

---
+++ aula02/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dataRoutes from "./routes/route.js"; //data do route renomeado
+
+const App = express();
+const PORT = process.env.PORT || 3000;
+
+
+//Middleware para interpretar o JSON
+App.use(express.json());
+
+//Rota da API
+App.use('/', dataRoutes);
+
+App.listen(PORT, () => {
+    console.log("Servidor executando em locasHost:3000");
+});
